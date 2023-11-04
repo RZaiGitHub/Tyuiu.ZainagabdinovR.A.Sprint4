ddc317e1aaedb28f7222b2708d89f5b30e0a6252
Tyuiu.ZainagabdinovR.A@outlook.com <Ruslanzai2003@gmail.com>
2023-11-04 18:34:04 +0500

    Add project files.

---
+++ Tyuiu.ZainagabdinovR.A.Sprint4.Task0.V30/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZainagabdinovR.A.Sprint4.Task0.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
